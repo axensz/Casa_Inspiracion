377ec081069f875b0d658f1e50b3f1c37700ad61
Camilo Guzmán <89235206+axensz@users.noreply.github.com>
2022-05-09 10:41:53 -0500

    Version 9.6.2

--- php/artephp/db_config.php
+++ php/artephp/db_config.php
@@ -1,9 +1,11 @@
 <?php
-    $conn =mysqli_connect(
-        'localhost',
-        'root',
-        '',
-        'galeriacasainspiracion',
-        '3307'
-    );
+
+    $mysqli = new mysqli("localhost", "root", "", "login_registro_db");
+    /*
+    if ($mysqli) {
+        echo 'conexion exitosa';
+    }else{
+        echo 'Conexion no ha sido exitosa';
+    }
+    */
 ?>
