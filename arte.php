fd029593461ab44f2958f0b4ebff230e36dc70a0
Camilo Guzmán <89235206+axensz@users.noreply.github.com>
2022-05-10 02:34:34 -0500

    Version 10.1.6

--- arte.php
+++ arte.php
@@ -8,11 +8,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="icon" href="img/Logos/PNG/LogoPNG.png">
     <link rel="stylesheet" href="css/style_arte.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="img/Logos/PNG/LogoPNG.png">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Arte</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
 </head>
 <body>
     <section class="header">
@@ -24,6 +28,77 @@
             <a href="#arte" class="hero-btn">Ver las galerías</a>
     </section>
     <div class="arte" id="arte">
+        <div class="container">
+            <h3 class="titulo"><i><b>GALERÍA</b></i></h3>
+            <!--Mensaje de error-->
+            <form action="php/image_gallary/subir_imagen.php" class="form-image-upload" method="POST" enctype="multipart/form-data">
+                <?php if (!empty($_SESSION['error'])) { ?>
+                    <div class="alert alert-danger">
+                        <strong>Error</strong>: Ha ocurrido un problema al subir la imagen <br><br>
+                        <ul>
+                            <li><?php echo $_SESSION['error']; ?></li>
+                        </ul>
+                    </div>
+                <?php unset($_SESSION['error']);
+                } ?>
+                <!--No ha ocurrido ningun problema -->
+                <?php if (!empty($_SESSION['success'])) { ?>
+                    <div class="alert alert-success alert-block">
+                        <button type="button" class="close" data-dismiss="alert">×</button>
+                        <strong><?php echo $_SESSION['success']; ?></strong>
+                    </div>
+                <?php unset($_SESSION['success']);
+                } ?>
+
+                <div class="row">
+                    <div class="col-md-5">
+                        <strong>Titulo:</strong>
+                        <input type="text" name="title" class="form-control" placeholder="Titulo">
+                    </div>
+                    <div class="col-md-5">
+                        <strong>Imagen:</strong>
+                        <input type="file" name="image" class="form-control">
+                    </div>
+                    <div class="col-md-2">
+                        <br />
+                        <button type="submit" class="btn btn-success">Subir archivo</button>
+                    </div>
+                </div>
+            </form>
+
+            <div class="row">
+                <div class='list-group gallery'">
+                    <?php
+                    require('php/image_gallary/db_config.php');
+
+                    $sql = "SELECT * FROM image_gallery";
+                    $images = $conn->query($sql);
+
+                    while ($image = $images->fetch_assoc()) {
+
+                    ?>
+                        <div class='col-sm-3'>
+
+                            <a class="thumbnail fancybox" rel="ligthbox" href="php/image_gallary/imagenes_subidas/<?php echo $image['image'] ?>">
+                            
+                                <img alt="" src="php/image_gallary/imagenes_subidas/<?php echo $image['image'] ?>" />
+                                <div class='text-center'>
+                                    <small class='text-muted'><?php echo $image['title'] ?></small>
+                                </div> <!--Texto central y final-->
+                            </a>
+
+                            <!--Eliminar imagen-->
+                            <form action="php/image_gallary/eliminar_imagen.php" method="POST">
+                                <input type="hidden" name="id" value="<?php echo $image['id'] ?>">
+                                <button type="submit" title="delete" class="close-icon btn btn-danger"><i class="glyphicon glyphicon-remove"></i></button>
+                            </form>
+
+                        </div>
+                    <?php } ?>
+
+                </div>
+            </div>
+        </div> 
     </div>    
 
     <footer class="footer2">
@@ -44,3 +119,12 @@
 
 </body>
 </html>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $(".fancybox").fancybox({
+            openEffect: "none",
+            closeEffect: "none",
+        });
+    });
+</script>
