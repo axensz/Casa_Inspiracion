4852d2384b56b4a0508664ef4eb8c213684fb2b3
Camilo Guzmán <89235206+axensz@users.noreply.github.com>
2022-05-22 16:44:02 -0500

    Version 11.2.3.1

--- editar_perfil.php
+++ editar_perfil.php
@@ -49,26 +49,32 @@
             </header>
             <div class='dashboard-contenido'>
                 <div class='container'>
-                    <div class="container rounded bg-white mt-5 mb-5">
-                        <div class="row">
-                            <div class="col-md-3 border-right">
-                                <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"><span class="font-weight-bold">Edogaru</span><span class="text-black-50"><?php echo $correo ?></span><span> </span></div>
-                            </div>
-                            <div class="col-md-5 border-right">
-                                <div class="p-3 py-5">
-                                    <div class="d-flex justify-content-between align-items-center mb-3">
-                                        <h4 class="text-right" id="tittle">Editar Perfil</h4>
+                    <div class="editarperfil">
+                        <div class="container rounded bg-white mt-5 mb-5" >
+                            <div class="row">
+                                <div class="col-md-3 border-right">
+                                    <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg">
+                                    <span class="font-weight-bold"><?php echo $nombre ?></span>
+                                    <span class="text-black-50"></span>
+                                    <span></span>
+                                </div>
+                                </div>
+                                <div class="col-md-5 border-right">
+                                    <div class="p-3 py-5">
+                                        <div class="d-flex justify-content-between align-items-center mb-3">
+                                            <h4 class="text-right" id="tittle">Editar Perfil</h4>
+                                        </div>  
+                                        <div class="row mt-2">
+                                            <div class="col-md-12"><label class="labels">Nombre</label><input type="text" class="form-control" placeholder="Nombre completo" value="" ></div>
+                                            <div class="col-md-12"><label class="labels">Email</label><input type="text" class="form-control" placeholder="Email" value="" readonly="readonly"></div>
+                                            <div class="col-md-12"><label class="labels">Contraseña</label><input type="text" class="form-control" placeholder="Contraseña" value=""></div>
+                                        </div>
+                                        <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Guardar edición</button></div>
                                     </div>
-                                    <div class="row mt-2">
-                                        <div class="col-md-12"><label class="labels">Nombre</label><input type="text" class="form-control" placeholder="Nombre completo" value=""></div>
-                                        <div class="col-md-12"><label class="labels">Email</label><input type="text" class="form-control" placeholder="Email" value=""></div>
-                                        <div class="col-md-12"><label class="labels">Telefono</label><input type="text" class="form-control" placeholder="telefonico" value=""></div>
-                                    </div>
-                                    <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div>    
                 </div>
             </div>
         </div>
