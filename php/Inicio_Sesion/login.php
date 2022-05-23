7dfcd7885bc5aa2c51b494ac0a40ef98bbaf7f2a
Camilo Guzmán <89235206+axensz@users.noreply.github.com>
2022-05-23 01:32:56 -0500

    Version 11.2.6

--- php/Inicio_Sesion/login.php
+++ php/Inicio_Sesion/login.php
@@ -6,7 +6,7 @@
     if ($_POST) {
         $correo = $_POST['correo'];
         $password = $_POST['password'];
-        $password_bd = hash('sha512', $password);
+        //$password = hash('sha512', $password);
 
         $sql = "Select id, password, tipo_usuario, nombre, correo, ocupacion, telefono FROM db_login WHERE correo = '$correo'";
         //echo $sql;
@@ -16,13 +16,18 @@
         if ($num>0) {
             $row = $resultado->fetch_assoc();
             $password_bd = $row['password'];
-            $_SESSION['correo'] = $row['correo'];
-            $_SESSION['id'] = $row['id'];
-            $_SESSION['nombre'] = $row['nombre'];
-            $_SESSION['tipo_usuario'] = $row['tipo_usuario'];
-            $_SESSION['ocupacion'] = $row['ocupacion'];
-            $_SESSION['telefono'] = $row['telefono'];
-            header("Location: index.php");
+            if ($password == $password_bd) {
+                $_SESSION['correo'] = $row['correo'];
+                $_SESSION['id'] = $row['id'];
+                $_SESSION['nombre'] = $row['nombre'];
+                $_SESSION['tipo_usuario'] = $row['tipo_usuario'];
+                $_SESSION['ocupacion'] = $row['ocupacion'];
+                $_SESSION['telefono'] = $row['telefono'];
+                header("Location: index.php");
+            }else{
+                $error = 1;
+            }
+            
         }else{
             $error = 1;
         }
